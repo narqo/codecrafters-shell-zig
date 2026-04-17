21cd7acd30b487eaf9b28d6220430bb6a5d3d2af
Vladimir Varankin <vladimir@varank.in>
2026-04-17 23:38:32 +0200

    handle invalid command

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,18 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    var file_writer = std.Io.File.stdout().writer(io, &.{});
-    try file_writer.interface.writeAll("$ ");
+
+    var writer = std.Io.File.stdout().writer(io, &.{});
+    const stdout = &writer.interface;
+
+    try stdout.writeAll("$ ");
+
+    var buf: [1024]u8 = undefined;
+    var reader = std.Io.File.stdin().reader(io, &buf);
+    const stdin = &reader.interface;
+
+    const cmd = try stdin.takeDelimiterExclusive('\n');
+
+    try stdout.print("{s}: command not found\n", .{cmd});
+    try stdout.flush();
 }
