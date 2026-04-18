fe94b0a242cf0e256d7f5cf0ca922208941090ee
Vladimir Varankin <vladimir@varank.in>
2026-04-18 21:59:47 +0200

    init zig build

--- build.zig
+++ build.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "test-zig-shell",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(exe);
 }
