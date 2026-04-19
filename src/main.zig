623a65c43388ac82a98dc5e338e8b5de98bfe588
Vladimir Varankin <vladimir@varank.in>
2026-04-19 21:19:26 +0200

    extract findExecutable

--- src/main.zig
+++ src/main.zig
@@ -20,7 +20,6 @@ const Cmd = enum {
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    const alloc = init.gpa;
 
     const cwd = std.Io.Dir.cwd();
 
@@ -45,6 +44,13 @@ pub fn main(init: std.process.Init) !void {
             }
         };
 
+        var arena = std.heap.ArenaAllocator.init(init.gpa);
+        defer arena.deinit();
+
+        const alloc = arena.allocator();
+
+        const path_env = init.environ_map.get("PATH") orelse "";
+
         const cmd = Cmd.fromString(cmd_str) orelse {
             try stdout.print("{s}: command not found\n", .{line});
             try stdout.flush();
@@ -57,32 +63,35 @@ pub fn main(init: std.process.Init) !void {
                 try stdout.print("{s}\n", .{args});
             },
             .type => {
-                var found = false;
                 if (Cmd.fromString(args)) |_| {
-                    found = true;
                     try stdout.print("{s} is a shell builtin\n", .{args});
-                } else if (init.environ_map.get("PATH")) |path_env| {
-                    var paths_iter = std.mem.splitScalar(u8, path_env, std.fs.path.delimiter);
-                    while (paths_iter.next()) |path_str| {
-                        if (path_str.len == 0) continue;
-
-                        const exe_path = try std.fs.path.join(alloc, &[_][]const u8{ path_str, args });
-                        defer alloc.free(exe_path);
-
-                        const exe_stat = cwd.statFile(io, exe_path, .{}) catch continue; // ignore any errors and move on
-                        if ((exe_stat.permissions.toMode() & std.posix.X_OK) != 0) {
-                            found = true;
-                            try stdout.print("{s} is {s}\n", .{ args, exe_path });
-                            break;
-                        }
-                    }
-                }
-                if (!found) {
-                    try stdout.print("{s}: not found\n", .{args});
+                } else if (findExecutable(alloc, io, cwd, path_env, args)) |exe_path| {
+                    try stdout.print("{s} is {s}\n", .{ args, exe_path });
+                } else |err| switch (err) {
+                    FindError.NotFound => try stdout.print("{s}: not found\n", .{args}),
+                    else => return err,
                 }
             },
         }
 
         try stdout.flush();
     }
+}
+
+const FindError = error{NotFound};
+
+fn findExecutable(alloc: std.mem.Allocator, io: std.Io, cwd: std.Io.Dir, path_env: []const u8, args: []const u8) ![]const u8 {
+    var paths_iter = std.mem.splitScalar(u8, path_env, std.fs.path.delimiter);
+    while (paths_iter.next()) |path_str| {
+        if (path_str.len == 0) continue;
+
+        const exe_path = try std.fs.path.join(alloc, &[_][]const u8{ path_str, args });
+        defer alloc.free(exe_path);
+
+        const exe_stat = cwd.statFile(io, exe_path, .{}) catch continue; // ignore any errors and move on
+        if ((exe_stat.permissions.toMode() & std.posix.X_OK) != 0) {
+            return alloc.dupe(u8, exe_path);
+        }
+    }
+    return FindError.NotFound;
 }
