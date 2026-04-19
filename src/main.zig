e571a8601b14ae55b8ad02971aef55621c0c6d91
Vladimir Varankin <vladimir@varank.in>
2026-04-19 20:37:33 +0200

    cmd type: locate executable

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,9 @@ const Cmd = enum {
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
+    const alloc = init.gpa;
+
+    const cwd = std.Io.Dir.cwd();
 
     var writer = std.Io.File.stdout().writer(io, &.{});
     const stdout = &writer.interface;
@@ -54,9 +57,27 @@ pub fn main(init: std.process.Init) !void {
                 try stdout.print("{s}\n", .{args});
             },
             .type => {
+                var found = false;
                 if (Cmd.fromString(args)) |_| {
+                    found = true;
                     try stdout.print("{s} is a shell builtin\n", .{args});
-                } else {
+                } else if (init.environ_map.get("PATH")) |path_env| {
+                    var paths_iter = std.mem.splitScalar(u8, path_env, std.fs.path.delimiter);
+                    while (paths_iter.next()) |path_str| {
+                        if (path_str.len == 0) continue;
+
+                        const exe_path = try std.fs.path.join(alloc, &[_][]const u8{ path_str, args });
+                        defer alloc.free(exe_path);
+
+                        const exe_stat = cwd.statFile(io, exe_path, .{}) catch continue; // ignore any errors and move on
+                        if ((exe_stat.permissions.toMode() & std.posix.X_OK) != 0) {
+                            found = true;
+                            try stdout.print("{s} is {s}\n", .{ args, exe_path });
+                            break;
+                        }
+                    }
+                }
+                if (!found) {
                     try stdout.print("{s}: not found\n", .{args});
                 }
             },
